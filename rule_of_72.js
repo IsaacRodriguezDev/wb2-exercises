eec8cf13a0e38da31be88e76f6349575f15bda87
IsaacRodriguezDev <isaacrodriguez.dev@gmail.com>
2024-04-24 13:35:35 -0500

    a few additions

--- rule_of_72.js
+++ rule_of_72.js
@@ -1,8 +1,8 @@
-let annualRateReturn = 6
+let annualRateReturn = 6.25
 let rule = 72
-let accountBalance = 650.30
+let accountBalance = 650.34
 
 let doubledAccountBalance = accountBalance.toFixed(2) * 2
 let yearsToDouble = rule / annualRateReturn
 
-console.log("Your account balance is currently "+"$"+accountBalance+" and with a "+ annualRateReturn+'% interest rate, your savings account will be worth $'+ doubledAccountBalance+ ' in ' + yearsToDouble+' years')
+console.log("Your account balance is currently "+"$"+accountBalance+" and with a "+ annualRateReturn+'% interest rate, your savings account will be worth $'+ doubledAccountBalance+ ' in ' + yearsToDouble.toFixed(1)+' years')
