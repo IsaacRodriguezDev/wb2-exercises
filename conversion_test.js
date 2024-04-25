fbda2549312566be8545f80be15e336c58679124
IsaacRodriguezDev <isaacrodriguez.dev@gmail.com>
2024-04-25 13:23:14 -0500

    completed conversion_test.js

---
+++ conversion_test.js
@@ -0,0 +1,16 @@
+"use strict";
+let a = "101.1";
+let b = "55";
+let c = "402 stevens";
+let d = "Number 5";
+// convert using parseInt()
+console.log("parseInt: " + parseInt(a), parseInt(b), parseInt(c), parseInt(d));
+
+// convert using parseFloat()
+console.log('parseFloat: '+parseFloat(a), parseFloat(b), parseFloat(c), parseFloat(d));
+
+// convert using Number()
+console.log('Number: '+Number(a), Number(b), Number(c), Number(d));
+
+// convert using urinary
+console.log('urinary: '+ +a, +b, +c, +d);
---
+++ test.js
@@ -0,0 +1,22 @@
+"use strict"
+// let object1 = [3, 6]
+// let object2 = [5, 9]
+
+// let object = ((object1[1]-object1[0])**2+(object2[1]-object2[0])**2)**0.5
+
+// console.log(object)
+
+// let x = 22.6948565656
+
+// console.log(x)
+// console.log(x.toFixed(4))
+
+
+let num = '5';
++num
+console.log(num)
+let newNum = ++num ;
+console.log('newnum :'+newNum);
+console.log('original num :'+num)
+
+
