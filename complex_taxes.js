a49cbfde3826867f25895802e2d681153cdf9e85
IsaacRodriguezDev <isaacrodriguez.dev@gmail.com>
2024-04-26 14:09:08 -0500

    completed complex_taxes.js

--- complex_taxes.js
+++ complex_taxes.js
@@ -1,25 +1,53 @@
-let filingStatus = 'Single'
-let annualGrossIncome = 0
-
-let taxesPercent = 0.23
-let weeklyIncome = 2350.65
-let weeklyTaxes = weeklyIncome*taxesPercent
-let weeklyIncomeAfterTax = (weeklyIncome - weeklyTaxes) 
-
-annualGrossIncome = weeklyIncomeAfterTax * 52
-console.log(annualGrossIncome.toFixed(2))
+let payRate = 22.42
+let hoursWorked = 58
+let regularPay = payRate * (40)
+let overtimePay = payRate * ((hoursWorked - 40)*1.5)
+let annualGrossPay = regularPay + overtimePay
+let filingStatus = 'Joint'
+annualGrossPay = annualGrossPay * 52
+console.log(annualGrossPay.toFixed(2))
 
 if (filingStatus == 'Single'){
-    if(annualGrossIncome < 12000){
-        annualGrossIncome = annualGrossIncome * (1.00-0.05)
-    } else if(annualGrossIncome >= 12000 && annualGrossIncome < 25000){
-    annualGrossIncome = annualGrossIncome * (1.00-0.10)
+    if(annualGrossPay < 12000){
+        taxWithholdings = annualGrossPay * (5/100)
+        annualGrossPay = annualGrossPay - taxWithholdings
+    } else if(annualGrossPay >= 12000 && annualGrossPay < 25000){
+    taxWithholdings = annualGrossPay * (10/100)
+    annualGrossPay = annualGrossPay - taxWithholdings
+} else if(annualGrossPay >= 25000 && annualGrossPay <75000){
+    taxWithholdings = annualGrossPay * (15/100)//(1.00-0.15)
+    annualGrossPay = annualGrossPay - taxWithholdings
+} else{
+    taxWithholdings = annualGrossPay * (20/100) //(1.00-(20/100))
+    annualGrossPay = annualGrossPay - taxWithholdings
+}
+} else //Joint status 
+{
+    if(annualGrossPay < 12000){
+        // annualGrossPay = annualGrossPay * (1.00-0.00)
+        taxWithholdings = annualGrossPay * (0.00)
+        annualGrossPay = annualGrossPay - taxWithholdings
 
-} else if(annualGrossIncome >= 25000 && annualGrossIncome <75000){
-    annualGrossIncome = annualGrossIncome * (1.00-0.15)
+    } else if(annualGrossPay >= 12000 && annualGrossPay < 25000){
+       
+        taxWithholdings = annualGrossPay * (0.06)
+        annualGrossPay = annualGrossPay - taxWithholdings
+
+} else if(annualGrossPay >= 25000 && annualGrossPay <75000){
+    taxWithholdings = annualGrossPay * (0.11)
+
+    annualGrossPay = annualGrossPay - taxWithholdings
 
 } else{
-    annualGrossIncome = (annualGrossIncome)* (1.00-0.20)
+    taxWithholdings = annualGrossPay * (20/100) //(1.00-(20/100))
+    annualGrossPay = annualGrossPay - taxWithholdings
 }
 }
-console.log(annualGrossIncome.toFixed(2))
+
+console.log(`You worked:${hoursWorked} hours this period.`)
+console.log(`Because you earn ${payRate} per hour, your gross pay is $${annualGrossPay.toFixed(2)}`)
+console.log(`Your filing status is ${filingStatus}`)
+console.log(`Your tax withholdings this period is $${taxWithholdings.toFixed(2)}`)
+
+console.log(`Your net pay is $${annualGrossPay.toFixed(2)}`)
+
