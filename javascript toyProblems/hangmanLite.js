38cc1fbcfb0e27e8860b24ea9440d4f4acd39693
IsaacRodriguezDev <isaacrodriguez.dev@gmail.com>
2024-04-26 15:53:54 -0500

    fixed some hangmanLite.js issues

--- javascript toyProblems/hangmanLite.js
+++ javascript toyProblems/hangmanLite.js
@@ -1,17 +1,20 @@
 let word = 'hangman'
-let include = /[h,a]/gi
-let letter
-letter = word.match(include)
-console.log(letter);
-if(letter.includes(...word)){
+let letter = /[n]/gi
+
+//word.match(include)
+console.log(` :${word.match(letter)} `);
+if(word.match(letter)){
+    letter = word.match(letter)
     let times = letter.length
     console.log('Yeah, the letter '+letter+' exists '+times+' times in my word')
 
 }else{
     console.log('Nope, that letter doesn\'t exist in my word.')
 
-}
+ }
 // let str = 'you smell really bad'
 // let exp = /[y,o,b,d]/gi
 // let match = str.match(exp)
 // console.log(match.length);
+
+
